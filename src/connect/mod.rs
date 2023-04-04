ab85605655fa8db57670d536fb966e3b9b9aa219
jonkarrer <karrerjon@gmail.com>
2023-04-03 22:15:42 -0400

    refactor: connect mod Query struct and impl

--- src/connect/mod.rs
+++ src/connect/mod.rs
@@ -1,37 +1,28 @@
 use dotenv::dotenv;
 
-struct Query {
-  base_url: String, // This is static
-  api_type: String, // Required, make Enum
-  stock_symbol: String, // Required
-  timeframe: String, // Required, make Enum
-  start: Option<String>, // Optional
-  end: Option<String>, // Optional
-  limit: u32, // Optional
-  page_token: Option<String>, // Optional
-  asof: Option<String>, // Optional
-  adjustment: Option<String>, // Optional
-  feed: Option<String> // Optional, default is free version
+pub struct Query {
+    pub api_type: String, // Required, make Enum
+    pub stock_symbol: String,
+    pub query_string: String
 }
 
 // ? Should maybe make the Bars api type an extenstion of query, and maybe even each version of query.
 // .. Maybe make query the root of all extensions for the request to the api.
 impl Query {
-  fn new(stock_symbol: String, api_type: String ) -> Self {
-    Query {
-      base_url: String::from("https://data.alpaca.markets/v2/stocks"),
-      api_type,
-      stock_symbol,
+    pub fn send(&self) -> serde_json::Value {
+      dotenv().ok();
+    
+      let id_key = ("APCA-API-KEY-ID", &std::env::var("APCA_API_KEY_ID").unwrap()); 
+      let secret_key = ("APCA-API-SECRET-KEY", &std::env::var("APCA_API_SECRET_KEY").unwrap());
+      let base_url = "https://data.alpaca.markets/v2/stocks";
+      let Self {api_type, stock_symbol, query_string } = &self;
+      let path = format!("{base_url}/{stock_symbol}/{api_type}?{query_string}");
+
+      return ureq::get(&path)
+        .set(id_key.0, id_key.1)
+        .set(secret_key.0, secret_key.1)
+        .call().unwrap()
+        .into_json().unwrap();
     }
-  }
 }
 
-pub fn send_query() -> serde_json::Value {
-  dotenv().ok();
-  
-  return ureq::get("https://data.alpaca.markets/v2/stocks/SO/bars?timeframe=1Week&start=2023-01-03")
-    .set("APCA-API-KEY-ID", &std::env::var("APCA_API_KEY_ID").unwrap())
-    .set("APCA-API-SECRET-KEY", &std::env::var("APCA_API_SECRET_KEY").unwrap())
-    .call().unwrap()
-    .into_json().unwrap();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use utils::connect_to_stock_api;
+use utils::Query;
 
 fn main() {
-  let json = connect_to_stock_api();
+    let query = Query {
+        api_type: String::from("bars"), // Required, make Enum
+        stock_symbol: String::from("SO"),
+        query_string: String::from("timeframe=1Week&start=2023-01-01")
+    };
+  let json = query.send();
   println!("{}", serde_json::to_string_pretty(&json).unwrap());
 }
