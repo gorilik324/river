5302b805f69cf3d8024f509ac9d891b22d1cc64b
jkarrer <jkarrer@protect247.com>
2023-04-10 14:45:11 -0400

    wip: ema calculation

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,25 @@ pub fn simple_moving_average(spread: &[f32]) -> f32 {
     // Divide by the length of the spread
     sum / spread.len() as f32
 }
+
+pub fn exponential_moving_average(spread: &[f32], period: usize) -> f32 {
+    let mut ema = 0.0;
+    let mut alpha = 2.0 / (period as f32 + 1.0);
+
+    for i in 0..period {
+        ema += spread[i];
+    }
+    ema /= period as f32;
+
+    for i in period..spread.len() {
+        ema = spread[i] * alpha + ema * (1.0 - alpha);
+    }
+
+    ema
+}
 fn main() {
-    let sma = simple_moving_average(&[23.2, 26.9, 32.88, 43.321, 20.0]);
-    println!("{sma}");
+    let data = vec![17.76, 17.48, 16.65, 17.7, 17.21, 17.49, 18.01, 18.85, 19.25, 18.78];
+    let period = 3;
+    let test = exponential_moving_average(&data, period);
+    println!("{test}");
 }
