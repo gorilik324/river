717b3f3e488723d2187a1937ad6b019815107703
jkarrer <jkarrer@protect247.com>
2023-04-11 10:34:51 -0400

    test: simple moving average function

--- src/indicators/simple_moving_average.rs
+++ src/indicators/simple_moving_average.rs
@@ -11,10 +11,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sma() {
-        let data = vec![4.0, 9.0, 15.0, 18.0, 21.0];
+    fn test_sma_wholes() {
+        let data = vec![4.21, 9.40, 15.0, 18.1, 21.0];
         let result = simple_moving_average(&data);
-        let expected = 13.4;
+        let expected = 13.542;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_sma_decimals() {
+        let data = vec![0.21, 0.40, 0.60, 0.15, 0.21];
+        let result = simple_moving_average(&data);
+        let expected = 0.314;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_sma_short() {
+        let data = vec![0.21];
+        let result = simple_moving_average(&data);
+        let expected = 0.21;
         assert_eq!(result, expected);
     }
 }
