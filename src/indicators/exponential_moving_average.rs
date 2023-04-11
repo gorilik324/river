522966660c7b78ad1d7b3d6e6f72b591f2652af3
jkarrer <jkarrer@protect247.com>
2023-04-11 11:28:11 -0400

    test: exponential moving average function

--- src/indicators/exponential_moving_average.rs
+++ src/indicators/exponential_moving_average.rs
@@ -1,4 +1,8 @@
 pub fn exponential_moving_average(spread: &[f32], period: usize) -> f32 {
+    if spread.len() < period {
+        panic!("Period For The EMA Is Out Of Bounds")
+    }
+
     let mut ema = 0.0;
     // Smoothing factor
     let mut alpha = 2.0 / (period as f32 + 1.0);
@@ -25,7 +29,7 @@ mod tests {
     fn test_ema_result() {
         let data: Vec<f32> = vec![
            10.0, 12.0, 13.0, 15.0, 16.0, 18.0, 17.0, 16.0, 19.0, 20.0, 22.0,
-           23.0, 25.0, 24.0, 22.0, 20.0, 18.0, 19.0, 21.0,23.0, 24.0,
+           23.0, 25.0, 24.0, 22.0, 20.0, 18.0, 19.0, 21.0, 23.0, 24.0,
            26.0, 27.0, 25.0, 23.0, 22.0, 20.0, 18.0, 19.0, 21.0, 23.0
         ];
 
@@ -33,5 +37,17 @@ mod tests {
         let expected = 21.329712;
         dbg!(&result);
         assert!((result-expected).abs() < 1e-6, "EMA of the dataset with period 5 is incorrect");
+    }
+
+    #[test]
+    #[should_panic(expected = "Period For The EMA Is Out Of Bounds")]
+    fn test_ema_error() {
+        let data: Vec<f32> = vec![
+           10.0, 12.0, 13.0, 15.0, 16.0, 18.0, 17.0, 16.0, 19.0, 20.0, 22.0,
+           23.0, 25.0, 24.0, 22.0, 20.0, 18.0, 19.0, 21.0,23.0, 24.0,
+           26.0, 27.0, 25.0, 23.0, 22.0, 20.0, 18.0, 19.0, 21.0, 23.0
+        ];
+
+        let result = exponential_moving_average(&data, 41);
     }
 }
